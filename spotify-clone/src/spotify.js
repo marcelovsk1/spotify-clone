254f18738dac9c95b5bab4e0dea61107ff58ba52
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-03 17:03:41 -0300

    working on

--- spotify-clone/src/spotify.js
+++ spotify-clone/src/spotify.js
@@ -1,4 +1,16 @@
 // https://developer.spotify.com/documentation
 // web-playback-sdk/quick-start/#
 
-export const authEndpoint = "https://accoutnts.spotify.com/authorize";
+export const authEndpoint =
+"https://accoutnts.spotify.com/authorize";
+
+const redirectUri = "https://localhost3000"
+const clientId = "8765545b25344f0095aa282cbadd8093"
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state"
+];
