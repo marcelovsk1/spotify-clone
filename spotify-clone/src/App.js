2b6c8d3c2004f9b3c782e45256250b516bce7bd5
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-03 15:59:25 -0300

    working on

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello we gonna start to building the spotify clone now!!!</h1>
+
+      {/* Spotify Logo */}
+      {/* Login with Spotify button */}
     </div>
   );
 }
