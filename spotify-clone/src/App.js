9423338c98b416189b1da30aad35a66799b3d820
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-03 16:41:18 -0300

    working on

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -5,10 +5,7 @@ import LoginView from './LoginView';
 function App() {
   return (
     <div className="App">
-      <h1>Hello we gonna start to building the spotify clone now!!!</h1>
 
-      {/* Spotify Logo */}
-      {/* Login with Spotify button */}
       <LoginView />
     </div>
   );
--- spotify-clone/src/LoginView.js
+++ spotify-clone/src/LoginView.js
@@ -4,12 +4,12 @@ import './LoginView.css';
 function Login() {
   return (
     <div className='loginView'>
-      <h1>Hi, I'm the login page</h1>
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"
-      alt="" className='SpotifyLogo' />
-      <button>Login with Spotify</button>
-      {/* Spotify logo */}
-      {/* Login with Spotify Button */}
+      alt="" className='SpotifyLogo'
+      />
+      {/* eslint-disable-next-line */}
+      <a href="#">Login With Spotify</a>
+
     </div>
   )
 }
