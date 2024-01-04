06041ca49c4cae4d6307636572ead773f57792ab
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-04 15:39:09 -0300

    working on

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,18 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import LoginView from './LoginView';
 import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
 
   // Run code based on a given condition
   useEffect(() => {
-    const token = getTokenFromUrl();
+    const hash = getTokenFromUrl();
     window.location.hash = "";
+    const _token = hash.access_token;
 
+    if (_token) {
+      setToken(_token)
+    }
 
     console.log('I have a token', token);
-  }, []);
+  }, [token]);
 
   return (
     <div className="App">
