3e64983898eb62dee9f13a1f0cc80c88ed06e45c
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-04 11:56:57 -0300

    working on

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import LoginView from './LoginView';
 
 function App() {
+
+  // Run code based on a given condition
+  useEffect(() => {
+    // code...
+  }, []);
+
   return (
     <div className="App">
 
