f74a77440a2aece6928385210c908a65f6148279
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-05 15:53:08 -0300

    working on

--- spotify-clone/src/index.js
+++ spotify-clone/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import DataLayer from './DataLayer'
+import { DataLayer } from './DataLayer'
 import reducer, { initialState } from './reducer';
 
 import reportWebVitals from './reportWebVitals';
